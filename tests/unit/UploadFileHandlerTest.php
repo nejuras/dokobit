fa9c1aaa4483e4540f07d1273485844fa46c2698
nerijus juras <nerusha@gmail.com>
2022-08-16 12:55:16 +0300

    update test

--- tests/unit/UploadFileHandlerTest.php
+++ tests/unit/UploadFileHandlerTest.php
@@ -2,49 +2,74 @@
 
 declare(strict_types=1);
 
-namespace Dokobit\Tests;
+namespace App\Tests;
 
 use Codeception\Test\Unit;
 use Doctrine\ORM\EntityManagerInterface;
+use Dokobit\Entity\UploadFileStatistics;
 use Dokobit\Model\UploadFile;
 use Dokobit\Model\UploadFileHandler;
-use Dokobit\Service\FileUploader;
-use Dokobit\Service\FileUploadInterface;
+use Dokobit\Repository\UploadFileStatisticsRepository;
+use PHPUnit\Util\Reflection;
 use Prophecy\PhpUnit\ProphecyTrait;
 use Prophecy\Prophecy\ObjectProphecy;
-use Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException;
+use SebastianBergmann\ObjectReflector\ObjectReflector;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Response;
 
 class UploadFileHandlerTest extends Unit
 {
+    public const IP_ADDRESS = '172.0.0.1';
+
     use ProphecyTrait;
 
     private UploadFileHandler $uploadFileHandler;
 
-    private FileUploadInterface|ObjectProphecy $fileUpload;
-
     private EntityManagerInterface|ObjectProphecy $entityManager;
+
+    private UploadFileStatisticsRepository|ObjectProphecy $repo;
 
     protected function setUp(): void
     {
         $this->entityManager = $this->prophesize(EntityManagerInterface::class);
-        $this->fileUpload = new FileUploader($this->getUploadedFileUrl());
+        $this->repo = $this->prophesize(UploadFileStatisticsRepository::class);
+
         $this->uploadFileHandler = new UploadFileHandler(
-            $this->fileUpload,
+            $this->repo->reveal(),
             $this->entityManager->reveal(),
         );
     }
 
+    /**
+     * @throws \ReflectionException
+     */
     public function testShouldGetApiResponse(): void
     {
-        $command = new UploadFile(['file' => $this->getUploadedFile()]);
+        $command = new UploadFile([
+            'file' => $this->getUploadedFile(),
+            'ipAddress' => self::IP_ADDRESS,
+        ]);
 
-        $this->expectException(FileNotFoundException::class);
+        $uploadFileStatistics = (new UploadFileStatistics())
+            ->setIpAddress(self::IP_ADDRESS)
+            ->setUsageCountPerDay(5);
+
+        (new \ReflectionClass(UploadFileStatistics::class))
+            ->getProperty('id')
+            ->setValue($uploadFileStatistics,2);
+
+        $this->repo->findOneBy(
+            [
+                'ipAddress' => self::IP_ADDRESS,
+            ]
+        )->shouldBeCalled()->willReturn($uploadFileStatistics);
+
 
         $result = $this->uploadFileHandler->__invoke($command);
 
-        $this->assertEquals(Response::HTTP_OK, $result);
+        $this->assertEquals(6, $result->getUsageCountPerDay());
+        $this->assertEquals(self::IP_ADDRESS, $result->getIpAddress());
+        $this->assertEquals(2, $result->getId());
 
     }
 
